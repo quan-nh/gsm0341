381c23eff71daff4fb2517d3be34333df1461717
QuanNH <huu2uan@gmail.com>
2013-05-23 15:20:15 +0700

    add report message decode

--- src/gsm0341/nokia/NOKIA.java
+++ src/gsm0341/nokia/NOKIA.java
@@ -6,6 +6,7 @@ import gsm0341.nokia.generated.CellIdListDisc;
 import gsm0341.nokia.generated.CellIdListDiscBase;
 import gsm0341.nokia.generated.CellList;
 import gsm0341.nokia.generated.CellList.CellListSequenceType;
+import gsm0341.nokia.generated.Report;
 import gsm0341.nokia.generated.Reset;
 import gsm0341.nokia.generated.StatusCBCHQuery;
 import gsm0341.util.CharsetTools;
@@ -18,9 +19,7 @@ import java.util.List;
 import org.bn.CoderFactory;
 import org.bn.IDecoder;
 import org.bn.IEncoder;
-import org.bn.annotations.ASN1Module;
 
-@ASN1Module(name = "NOKIA", isImplicitTags = true)
 public class NOKIA {
 
 	public static final byte[] lacAndCi = { (byte) 0x01 };
@@ -97,6 +96,32 @@ public class NOKIA {
 		encoder.encode(cbseApdus, outputStream);
 
 		return outputStream.toByteArray();
+	}
+
+	/**
+	 * Decode Report message.
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static Report decodeReport(byte[] data) throws Exception {
+		return decodeCBSEapdus(data).getCbseReport();
+	}
+
+	/**
+	 * Check if a message is report or not.
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public static boolean isReport(byte[] data) {
+		try {
+			CBSEapdus cbseAPDUs = decodeCBSEapdus(data);
+			return cbseAPDUs.isCbseReportSelected();
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	public static boolean isStatusCBCHQueryResp(byte[] data) {
@@ -178,5 +203,11 @@ public class NOKIA {
 		byte[] statusCBCHQueryResp = CharsetTools.hexStringToByteArray(cbchQueryResp);
 		System.out.println(cbchQueryResp);
 		System.out.println(isStatusCBCHQueryResp(statusCBCHQueryResp) ? "--StatusCBCHQueryResp" : "--not StatusCBCHQueryResp");
+
+		// test Report check
+		String reportMessage = "A3 80 80 02 04 D2 81 02 01 59 A2 80 30 80 A0 80 80 01 01 81 04 4E CA DA CB 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA CC 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA CD 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA D5 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA D6 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA D7 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA DF 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA E0 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA E1 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA E9 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA EA 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA EB 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA F3 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA F4 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA F5 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA FD 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA FE 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DA FF 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 07 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 08 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 09 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 11 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 12 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 13 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 1B 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 1C 00 00 81 01 00 00 00 30 80 A0 80 80 01 01 81 04 4E CA DB 1D 00 00 81 01 00 00 00 00 00 84 01 00 00 00";
+		byte[] report = CharsetTools.hexStringToByteArray(reportMessage);
+		System.out.println(reportMessage);
+		System.out.println(isReport(report) ? "--Report" : "--not Report");
 	}
 }
